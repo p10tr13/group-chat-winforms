e6df925b8bf546dc6757b0ab7dfa519862d039c7
Piotr Iśtok <160889621+p10tr13@users.noreply.github.com>
2024-04-07 16:36:28 +0200

    Ponowne dodanie projektu

---
+++ messageControl.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Messages;
+
+namespace WinFormsLab
+{
+    public partial class messageControl : UserControl
+    {
+        //https://stackoverflow.com/questions/32987649/how-to-create-a-user-control-with-rounded-corners
+
+        private int radius = 20;
+
+        public Messages.Message mess;
+
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                radius = value;
+                this.RecreateRegion();
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect,
+            int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private GraphicsPath GetRoundRectagle(Rectangle bounds, int radius)
+        {
+            float r = radius;
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(bounds.Left, bounds.Top, r, r, 180, 90);
+            path.AddArc(bounds.Right - r, bounds.Top, r, r, 270, 90);
+            path.AddArc(bounds.Right - r, bounds.Bottom - r, r, r, 0, 90);
+            path.AddArc(bounds.Left, bounds.Bottom - r, r, r, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void RecreateRegion()
+        {
+            var bounds = ClientRectangle;
+
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(bounds.Left, bounds.Top,
+                bounds.Right, bounds.Bottom, Radius, radius));
+            this.Invalidate();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.RecreateRegion();
+        }
+
+        public messageControl(string sender, string text, DateTime time)
+        {
+            InitializeComponent();
+            mess = new Messages.Message(sender, text, time);
+            nameLabel.Text = mess.Sender;
+            dateLabel.Text = mess.Time.ToString("HH:mm");
+            messageTextBox.Text = text;
+            nameLabel.Location = new Point(5, 5);
+            dateLabel.Location = new Point(this.Width - dateLabel.Width - 5, this.Height - dateLabel.Height - 5);
+            Anchor = AnchorStyles.Left | AnchorStyles.Top;
+        }
+    }
+}
